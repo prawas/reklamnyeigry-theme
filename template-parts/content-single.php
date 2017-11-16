75256802d21772da036679bf4bf74a19e7225ebc
Svetlana Tur <svetaltur@gmail.com>
2017-11-16 11:40:17 +0300

    поделиться.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -29,15 +29,21 @@
 	<div class="entry-content">
 		<?php
 			the_content( );
-
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kristinka' ),
 				'after'  => '</div>',
 			) );
 		?>
+
+			<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+<script src="//yastatic.net/share2/share.js"></script>
+<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,twitter"></div>
+
+			
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php kristinka_entry_footer(); ?>
+		
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
