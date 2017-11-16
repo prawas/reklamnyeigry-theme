5ca62ec3ba6790f0c5b676474e0ba6e8009d8dab
Svetlana Tur <svetaltur@gmail.com>
2017-11-16 12:02:30 +0300

    поправила кнопку "узнать больше" и форматирование.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -36,14 +36,14 @@
 		?>
 
 			<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-<script src="//yastatic.net/share2/share.js"></script>
-<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,twitter"></div>
+			<script src="//yastatic.net/share2/share.js"></script>
+			<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,twitter"></div>
 
 			
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php kristinka_entry_footer(); ?>
-		
+
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
