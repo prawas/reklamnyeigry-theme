30a147fa39c1d4fdc51a0a2c02d9c426a2a97d34
Svetlana Tur <svetaltur@gmail.com>
2017-11-21 17:13:12 +0300

    кнопка поделиться на одиночной записи - призы.

--- woocommerce.php
+++ woocommerce.php
@@ -16,10 +16,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		
 
 			<?php woocommerce_content(); ?>
 
+
 		</main><!-- #main -->
+
 	</div><!-- #primary -->
 
 <?php
