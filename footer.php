83949684e95b74a098cd89ea96a90b142e33f0e9
Svetlana Tur <svetaltur@gmail.com>
2017-11-22 09:53:18 +0300

    ссылка и версия.

--- footer.php
+++ footer.php
@@ -21,9 +21,9 @@
 		<div class="site-info">
 
       <div class="pull-right">
-        <a href="https://www.facebook.com/%D0%A0%D0%B5%D0%BA%D0%BB%D0%B0%D0%BC%D0%BD%D1%8B%D0%B5-%D0%B8%D0%B3%D1%80%D1%8B-%D0%B8-%D0%B0%D0%BA%D1%86%D0%B8%D0%B8-%D0%B2-%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D0%B8-417900291697759/" class="social-button facebook"><i class="fa fa-facebook"></i></a>
-        <a href="https://vk.com/club71569907" class="social-button vk"><i class="fa fa-vk"></i></a>
-        <a href="https://ok.ru/reklamnye" class="social-button ok"><i class="fa fa-odnoklassniki"></i></a>
+        <a href="https://www.facebook.com/%D0%A0%D0%B5%D0%BA%D0%BB%D0%B0%D0%BC%D0%BD%D1%8B%D0%B5-%D0%B8%D0%B3%D1%80%D1%8B-%D0%B8-%D0%B0%D0%BA%D1%86%D0%B8%D0%B8-%D0%B2-%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D0%B8-417900291697759/" class="social-button facebook"  target="_blank"><i class="fa fa-facebook"></i></a>
+        <a href="https://vk.com/club71569907" class="social-button vk"  target="_blank"><i class="fa fa-vk"></i></a>
+        <a href="https://ok.ru/reklamnye" class="social-button ok"  target="_blank"><i class="fa fa-odnoklassniki"></i></a>
       </div>
 
 			<a href="/">reklamnyeigry.by</a> &mdash; информационный портал о рекламных играх в Беларуси.
